dd105010e48c71967e250e8ea49abbe254e74d9d
vitrum <vitrum.cn@hotmail.com>
2011-11-01 11:17:28 +0800

    get time

--- serv03.js
+++ serv03.js
@@ -45,7 +45,7 @@ conn.addListener('message',function(msg){
 		client.query(
 		  'INSERT INTO '+TEST_TABLE+' '+
 		  'SET title = ?, text = ?, created = ?',
-		  [conn.id, msg, 'CURRENT_DATE()']
+		  [conn.id, msg, 'NOW()']
 		);
 
         var megContent = conn.id  
